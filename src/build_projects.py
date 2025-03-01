9c49218534909920bbd452252a5b14fd4d93525e
Yifan Mai <yifan@cs.stanford.edu>
2025-02-28 17:50:15 -0800

    Support NLP benchmark output storage

--- src/build_projects.py
+++ src/build_projects.py
@@ -11,6 +11,9 @@ window.{release_constant_name} = "{release}";
 window.BENCHMARK_OUTPUT_BASE_URL = "{benchmark_output_base_url}";
 window.PROJECT_ID = "{project_id}";
 """
+NLP_URL_TEMPLATE = (
+    "https://nlp.stanford.edu/helm/{project_id}/benchmark_output/"
+)
 GCS_URL_TEMPLATE = (
     "https://storage.googleapis.com/crfm-helm-public/{project_id}/benchmark_output/"
 )
@@ -35,19 +38,19 @@ def main():
         project_id = project["id"]
         project_path = os.path.join(DIST_PATH, project_id)
         os.makedirs(project_path, exist_ok=True)
-        latest_release = project["releases"][0]
+        latest_release = project["releases"][0] if project.get("releases") else project["preview_releases"][0]
         redirect_file_path = os.path.join(project_path, INDEX_FILE_NAME)
         with open(redirect_file_path, "w") as f:
             redirect_file_contents = REDIRECT_TEMPLATE.format(project_id=project_id)
             f.write(redirect_file_contents)
-        for release_or_latest in ["latest"] + project.get("preview_releases", []) + project["releases"]:
+        for release_or_latest in ["latest"] + project.get("preview_releases", []) + project.get("releases", []):
             release = latest_release if release_or_latest == "latest" else release_or_latest
             release_path = os.path.join(project_path, release_or_latest)
             os.makedirs(release_path, exist_ok=True)
             release_index_path = os.path.join(release_path, INDEX_FILE_NAME)
             shutil.copyfile(source_index_path, release_index_path)
             config_path = os.path.join(release_path, CONFIG_FILE_NAME)
-            benchmark_output_base_url = GCS_URL_TEMPLATE.format(project_id=project_id)
+            benchmark_output_base_url = NLP_URL_TEMPLATE.format(project_id=project_id) if project.get("benchmark_output_storage") == "nlp" else GCS_URL_TEMPLATE.format(project_id=project_id)
             config_contents = CONFIG_TEMPLATE.format(
                 release_constant_name="SUITE" if project.get("suites_only") else "RELEASE",
                 release=release,
